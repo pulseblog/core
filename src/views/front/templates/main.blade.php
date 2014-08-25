942186693bf0216b314e45b118053eea51b3f860
Zizaco <zizaco@gmail.com>
2014-08-24 22:31:09 -0300

    Added favicon

--- src/views/front/templates/main.blade.php
+++ src/views/front/templates/main.blade.php
@@ -5,8 +5,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
         <link rel="canonical" href="{{ Request::url() }}" />
-        <link href="/favicon.ico" type="image/x-icon" rel="shortcut icon"/>
-        <link href="/favicon.ico" type="image/x-icon" rel="icon"/>
+        <link href="packages/pulse/core/favicon.ico" type="image/x-icon" rel="shortcut icon"/>
+        <link href="packages/pulse/core/favicon.ico" type="image/x-icon" rel="icon"/>
         <title>{{ Config::get('pulse.blogname') }}{{ (Config::get('pulse.blogdesc') ? ' - '.Config::get('pulse.blogdesc') : '') }}</title>
         <link href="{{ asset('packages/pulse/core/assets/css/front.css') }}" rel="stylesheet">
     </head>
